a173f76f4776178c10a50113db9b8c5ab6c1bfcd
Marinah Tamura <marinahtam@gmail.com>
2023-06-28 20:46:28 -0700

    Creating a list of options that can be chosen through using inquirer

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const inquirer = require('inquirer');
-const questions = [
-    {
-        type: 'list',
-        message: '',
-        name: '',
-        choices: [],
-    }
-]
+const mysql = require("mysql2");
 
+function start () {
+    inquirer
+        .prompt({
+            type:"list",
+            name:"action",
+            message:"What would you like to do?",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "Update an employee role",
+            ],
+        })
+    }
 const prompt_questions = () => {
     inquirer
     .prompt(questions)
