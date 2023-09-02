fb1f7c50378daef5eea330c6ff4bfb591726cdfc
Marinah1031 <marinahtam@gmail.com>
2023-09-01 20:31:53 -0700

    description of the db for the sql

--- db/query.sql
+++ db/query.sql
@@ -1,5 +1,8 @@
+-- the part of the query select two columns
 SELECT department.name AS department, reviews.review
 FROM 
+-- retrieves all rows from the left table and matching rows from the right table based on the specified conditions
 LEFT JOIN department
 ON reviews.roles_id = department.id
+-- orders the result set by the 'department.name' column in ascending order.
 ORDER BY department.name;
--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,27 @@
+--This line drops the database named department_db if that exists
 DROP DATABASE IF EXISTS department_db;
 CREATE DATABASE department_db;
-
+--This line selects the database it wants to use
 USE department_db;
-
+--this defines an 'id' column as an integer, which is the primary key for the table
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
-
+--Auto_increment attribute means that MySQL will automaticlly assign a unique value to this column 
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    --creates a 'title' column for the role titles
     title VARCHAR(30) NOT NULL,
+    --This column stores salary information as a decimal value
     salary DECIMAL NOT NULL,
+    --stores the department ID to establish a relationship with the 'department' table. 
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department (id)
     ON DELETE CASCADE
 );
 
+--The creation of the employee table. 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
